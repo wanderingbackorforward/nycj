3055c1c855153be824f54212ca2651152b56e2ef
ynj <yangn497@gmail.com>
2026-05-20 00:39:25 +0800

    rename: cockpit -> md

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cockpit/',
+  base: '/md/',
   server: {
     port: 5174,
     host: '127.0.0.1',
